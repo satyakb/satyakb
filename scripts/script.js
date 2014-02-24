f3e991db8d9128398c58be593c7f1975e249a453
Satya Bodduluri <kowabungappl@gmail.com>
2014-02-23 22:15:54 -0500

    got rid of uneeded stuff. Add icon and document titles

--- scripts/script.js
+++ scripts/script.js
@@ -54,21 +54,6 @@
  	}, s, ease); 
  }
 
- //  function closeOuterPage() {
- //  	$('.inner').show();
- // 	$('.outer').show();
- // 	$('.outer').animate({
- // 		top: '0',
- // 		bottom: '0'
- // 	}, s, ease, function() {
- // 		$('.page').hide();
- // 	}); 
- // 	$('.inner').animate({
- // 		top: '0',
- // 		bottom: '0'
- // 	}, s, ease); 
- // }
-
  function showAbout() {
  	if (main) {
  		$('.page').hide();
@@ -77,6 +62,7 @@
  	}
  	$('.underline.on').removeClass('on').addClass('off').hide();
  	$('.navBarAbout').find('.underline').removeClass('off').addClass('on').show();
+ 	$('#aboutPage').css({'opacity': '1'});
  	if (main) {
  		$('#aboutPage').show();
  		main = false;
@@ -131,13 +117,6 @@
  }
 
  $(document).ready(function() {
-
- 	// setTimeout(function() {
- 	// 	if(!loaded) {
- 	// 		closeInnerPage();
- 	// 		loaded = true;
- 	// 	}
-  // 	}, 1000);
 
  	$('.panel').on('click', function(e) {
  		e.preventDefault();
@@ -229,118 +208,8 @@
  		var h = $(window).height();
  		$('.projPics').css({'height': h-200});
  		$('.contactContainer').css({'height': h-75});
- 	})
-
- 	// if (window.location.pathname == '/') {
- 	// 	console.log('noooo');
- 	// 	window.location.replace('/#');
- 	// }
-
- 	// if (window.location.pathname == '/#about') {
- 	// 	console.log('aboutttttt');
- 	// }
+ 	});
 
  	$(window).trigger('hashchange');
 
- 	// window.addEventListener('popstate', function(e) {
- 	// 	if (location.pathname == '/') {
- 	// 		location.replace('#home');
- 	// 	}
- 	// });
-
- 	// function setupHistoryClicks() {
- 	// 	addClicker($('#about'));
- 	// 	addClicker($('#work'));
- 	// }
-
- 	// function addClicker(link) {
- 	// 	link.on('click', function(e) {
- 	// 		console.log('whoo');
- 	// 		existing = true;
- 	// 		getPage(link.attr('href'));
- 	// 		history.pushState(null,null,link.attr('href'));
- 	// 		console.log(link.attr('href'));
- 	// 		e.preventDefault();
- 	// 	});
- 	// }
-
- 	// function getPage(href) {
- 	// 	var req = new XMLHttpRequest();
- 	// 	req.open("GET", href, false);
- 	// 	console.log(href);
- 	// 	req.send(null);
-		// if (req.status == 200) {
-		// 	console.log(req.responseText);
-		// 	$('body').append(req.responseText);
-		// 	$('.page').show();
-		// 	openInnerPage();
-		// 	return true;
-		// }
-		// return false;
- 	// }
-
-//  	xmlhttp.open("GET", url , true);
-// xmlhttp.onreadystatechange = function() {
-//     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//         var xmlDoc = xmlhttp.responseXML;
-//         // Do something here
-//     }
-// }
-// xmlhttp.send();
-
- // 	window.addEventListener("popstate", function(e) {
- //    	if (location.pathname == '/' || location.pathname == '/index.html') {
- //    		var req = new XMLHttpRequest();
- // 			req.open("GET", 'index.html', false);
- // 			req.send(null);
- // 			if (req.status == 200) {
- // 				if (side == 'inner') {
- // 					closeInnerPage();
- // 				} else if (side == 'outer') {
- // 					closeOuterPage();
- // 				}
- //    			console.log('whoooooooo');
- //    			return true;
- //  			}
- //    	}
-
- //    	if (location.pathname == '/about.html') {
- //    		var req = new XMLHttpRequest();
-	//  		req.open("GET", '/about.html', false);
-	//  		req.send(null);
-	// 		if (req.status == 200) {
-	// 			$('body').append(req.responseText);
-	// 			$('.page').show();
-	// 			openInnerPage();
-	// 			return true;
-	// 		}
- //    	}
-
- //    	if (location.pathname == '/work.html') {
- //    		var req = new XMLHttpRequest();
-	//  		req.open("GET", '/work.html', false);
-	//  		req.send(null);
-	// 		if (req.status == 200) {
-	// 			$('body').append(req.responseText);
-	// 			$('.page').show();
-	// 			openOuterPage();
-	// 			return true;
-	// 		}
- //    	}
- //    	return false;
-	// });
-
- // 	setupHistoryClicks();
-
- 	// $('#about').on('click', function(e) {
- 	// 	e.preventDefault();
- 	// 	$('#aboutPage').show();
- 	// 	openInnerPage();
- 	// 	history.pushState(data, event.target.textContent, event.target.href);
- 	// });
- 	// $('#work').on('click', function(e) {
- 	// 	e.preventDefault();
- 	// 	$('#workPage').show();
- 	// 	openOuterPage();
- 	// });
  });
